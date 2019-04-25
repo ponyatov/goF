35756321bf6cac5fb4f47e5f712575337798f3da
Dmitry Ponyatov <dponyatov@gmail.com>
2019-04-25 15:17:55 +0400

    v FVM packaged
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/main/goF.go
+++ src/main/goF.go
@@ -9,59 +9,28 @@ import (
 	"os"
 )
 
-/// main memory size, bytes
-const Msz = 0x1000
-
-/// instruction pointer
-var Ip = 0
-
-/// current instruction opcode
-var op byte = 0
-
-/// compiler/memory allocator pointer
-var Cp = 0
-
-/// return stack, cells
-const Rsz = 0x100
-
-/// return stack pointer
-var Rp = 0
-
-/// data stack, cells
-const Dsz = 0x10
-
-/// data stack pointer
-var Dp = 0
-
-type Stack struct {
-	data int
-	ptr  int
+type Stack struct { /// unused
+	size int   /// max size
+	data []int /// data container
+	top  int   /// top pointer
 }
-
-//var M byte[];
-
-func VM() {
-	//op = M[Ip] // fetch next command
-	Ip += 1 // autoincrement to next comment
-	switch op {
-	default:
-		os.Exit(-1)
-	}
-}
-
-func (s *Stack) push(value int) { s.data += value; s.ptr++ }
-
-// func (s Stack) push(value int)  { fmt.Println("pushmanda", s) } // no poly
 
 type FVM struct {
-	Msz int  /// memory size, bytes
-	Rsz int  /// return stack size, cells
-	Dsz int  /// data stack size, cells
-	Ip  int  /// instruction pointer
-	op  byte /// current opcode
-	Cp  int  /// compiler pointer
-	Dp  int  /// data stack pointer
-	Rp  int  /// return stack pointer
+	/// configuration
+	Msz int /// program/data memory size, bytes
+	Rsz int /// return stack size, cells
+	Dsz int /// data stack size, cells
+	/// VM registers & program/data memory
+	M  []byte
+	Ip int  /// instruction pointer
+	op byte /// current opcode
+	Cp int  /// compiler/memory allocator pointer
+	/// return stack
+	R  []int
+	Rp int /// pointer
+	/// data stack
+	D  []int
+	Dp int /// pointer
 }
 
 func NewFVM(M, R, D int) FVM {
@@ -71,18 +40,11 @@ func NewFVM(M, R, D int) FVM {
 		Cp: 0}
 }
 
+var vm = FVM{Msz: 0x1000, Rsz: 0x100, Dsz: 0x10}
+
 func main() {
-	x := Stack{}
-	fmt.Println(x)
-	x.push(11)
-	fmt.Println(x)
-	x.push(11)
-	fmt.Println(x)
-	power := 42
-	fmt.Println(os.Args, power, x)
+	fmt.Print(os.Args)
+	fmt.Println(vm)
 	lexer.Parse("# hello")
-	fmt.Println(NewFVM(Msz, Rsz, Dsz))
-	frame.Head(frame.NewFrame("hello", "world"))
-	fmt.Println(frame.Pool)
-	fmt.Println(frame.Frame{Tag: "tag", Val: "val"})
+	fmt.Println(frame.Frame{Tag: "hello", Val: "world"})
 }
